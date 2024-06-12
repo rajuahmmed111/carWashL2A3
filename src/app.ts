83c9847a2d1249bb18b46a20d03e8fe119b83a4f
rajuahmmed111 <ahraju2003@gmail.com>
2024-06-12 17:10:17 +0600

    first commit create Car Washing project

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
+const port = 3000;
+
+// parsers
+app.use(express.json());
+// app.use(cors({ origin: ["http://localhost:5173"] }));
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+export default app;
---
+++ src/app/config/index.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default {
+  NODE_ENV: process.env.NODE_ENV,
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_USER,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  default_pass: process.env.DEFAULT_PASS,
+  jwt_access_token: process.env.JWT_ACCESS_SECRET,
+  jwt_refresh_token: process.env.JWT_REFRESH_SECRET,
+  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
+  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+};
